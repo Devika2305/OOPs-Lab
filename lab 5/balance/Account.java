b35655ffab1542293520b90048d4873bc17575f0
Devika Gupta <57962316+Devika2305@users.noreply.github.com>
2021-06-12 18:36:56 +0530

    Add files via upload

---
+++ lab 5/balance/Account.java
@@ -0,0 +1,9 @@
+package balance;
+
+public class Account {
+	public void displayBalance(int b) {
+		System.out.println("Hello from displayBalance class");
+		System.out.println("Balance to be displayed = "+b);
+	}
+
+}
