203acea661ec2a10503dc0c1fb4079c51107e94d
Devika Gupta <57962316+Devika2305@users.noreply.github.com>
2021-06-13 11:46:48 +0530

    Add files via upload

---
+++ lab 5/sum/runCalc.java
@@ -0,0 +1,10 @@
+package sum;
+import sum.Calculator;
+
+public class runCalc {
+	public static void main(String args[]) {
+		Calculator obj =  new Calculator();
+		System.out.println(obj.add(100, 200));
+	}
+
+}
