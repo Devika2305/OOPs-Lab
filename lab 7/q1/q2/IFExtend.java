1c0df9cc1557f6fa33b3a231c06cad1048a26d79
Devika Gupta <57962316+Devika2305@users.noreply.github.com>
2021-06-15 19:40:14 +0530

    Add files via upload

---
+++ lab 7/q1/q2/IFExtend.java
@@ -0,0 +1,10 @@
+package com.R177219073.q2;
+
+public class IFExtend {
+	public static void main(String[] args) {
+		MyClass obj = new MyClass();
+		obj.meth1();
+		obj.meth2();
+		obj.meth3();
+	}
+}
---
+++ lab 7/q1/q2/MyClass.java
@@ -0,0 +1,17 @@
+package com.R177219073.q2;
+
+public class MyClass implements B
+{
+	public void meth1()
+	{
+		System.out.println("Implement meth1().");
+	}
+	public void meth2()
+	{
+		System.out.println("Implement meth2().");
+	}
+	public void meth3()
+	{
+		System.out.println("Implement meth3().");
+	}
+}
