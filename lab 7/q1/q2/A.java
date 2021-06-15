e317752cb5b36479bd74bc759907883f6f37726d
Devika Gupta <57962316+Devika2305@users.noreply.github.com>
2021-06-15 19:38:35 +0530

    Add files via upload

---
+++ lab 7/q1/q2/A.java
@@ -0,0 +1,6 @@
+package com.R177219073.q2;
+
+public interface A {
+	void meth1();
+	void meth2();
+}
---
+++ lab 7/q1/q2/B.java
@@ -0,0 +1,6 @@
+package com.R177219073.q2;
+
+public interface B extends A
+{
+	void meth3();
+}
